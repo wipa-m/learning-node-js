c18dd689dacec4f87af9d64d94b74e5ba6c067f2
wipa-m <wipawadee.mk@gmail.com>
2022-07-23 22:44:22 +0700

    update template web

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const express = require("express");
 const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 const port = 3001;
 
 app.use(morgan('combined'))
-// เข้ามาใน path / แแล้วจะส่งอะไรกลับไปให้
-app.get("/", (req, res) => {
-  res.send("Hello Fernkyouya ja");
-});
+//use html website
+app.use(express.static(path.join(__dirname,"/public/")))
+// เข้ามาใน path / แแล้วจะส่งอะไรกลับไปให้ หน้าเว็บ
+// app.get("/", (req, res) => {
+//   res.send("Hello Fernkyouya ja");
+// });
 
 app.listen(port, () => {
   // console.log("listening on port : " + chalk.green(port));
